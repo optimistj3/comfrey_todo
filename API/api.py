0e9cdfcbd4772d9e7ead61598f843031fe8affeb
henne <h.vandervegt@student.fontys.nl>
2019-06-18 23:38:27 +0200

    added some stuff

--- API/api.py
+++ API/api.py
@@ -1,1 +1,17 @@
-from flask import Flask, request
+from flask import Flask, request, url_for, redirect, session
+from flaskext.mysql import MySQL
+from passlib.hash import pbkdf2_sha256 as sha
+
+import datetime
+import jwt
+import logging
+import requests
+
+@app.route("/api/item")
+def item():
+
+@app.route("/api/user")
+def user():
+
+@app.route("/api/list")
+def lists():
